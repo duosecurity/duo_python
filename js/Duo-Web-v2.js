65d9524b743c20bca6f0e32071442d40fcb826b9
Geoff Keyes <gkeyes@duosecurity.com>
2017-05-22 17:44:59 -0400

    Update Duo-Web-v2.js to the latest version: v2.6
    The JS file in the repo is currently 3 versions behind (v2.3), and
    this commit will get it up to the current version of v2.6.
    v2.6 is needed for trusted mobile endpoints functionality.

--- js/Duo-Web-v2.js
+++ js/Duo-Web-v2.js
@@ -1,10 +1,38 @@
 /**
  * Duo Web SDK v2
- * Copyright 2015, Duo Security
+ * Copyright 2017, Duo Security
  */
-window.Duo = (function(document, window) {
+
+(function (root, factory) {
+    /*eslint-disable */
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    /*eslint-enable */
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        var Duo = factory();
+        // If the Javascript was loaded via a script tag, attempt to autoload
+        // the frame.
+        Duo._onReady(Duo.init);
+
+        // Attach Duo to the `window` object
+        root.Duo = Duo;
+  }
+}(this, function() {
     var DUO_MESSAGE_FORMAT = /^(?:AUTH|ENROLL)+\|[A-Za-z0-9\+\/=]+\|[A-Za-z0-9\+\/=]+$/;
     var DUO_ERROR_FORMAT = /^ERR\|[\w\s\.\(\)]+$/;
+    var DUO_OPEN_WINDOW_FORMAT = /^DUO_OPEN_WINDOW\|/;
+    var VALID_OPEN_WINDOW_DOMAINS = [
+        'duo.com',
+        'duosecurity.com',
+        'duomobile.s3-us-west-1.amazonaws.com'
+    ];
 
     var iframeId = 'duo_iframe',
         postAction = '',
@@ -149,7 +177,8 @@ window.Duo = (function(document, window) {
             typeof event.data === 'string' &&
             (
                 event.data.match(DUO_MESSAGE_FORMAT) ||
-                event.data.match(DUO_ERROR_FORMAT)
+                event.data.match(DUO_ERROR_FORMAT) ||
+                event.data.match(DUO_OPEN_WINDOW_FORMAT)
             )
         );
     }
@@ -214,7 +243,7 @@ window.Duo = (function(document, window) {
             }
 
             if (options.iframe) {
-                if ('tagName' in options.iframe) {
+                if (options.iframe.tagName) {
                     iframe = options.iframe;
                 } else if (typeof options.iframe === 'string') {
                     iframeId = options.iframe;
@@ -256,12 +285,50 @@ window.Duo = (function(document, window) {
      */
     function onReceivedMessage(event) {
         if (isDuoMessage(event)) {
-            // the event came from duo, do the post back
-            doPostBack(event.data);
+            if (event.data.match(DUO_OPEN_WINDOW_FORMAT)) {
+                var url = event.data.substring("DUO_OPEN_WINDOW|".length);
+                if (isValidUrlToOpen(url)) {
+                    // Open the URL that comes after the DUO_WINDOW_OPEN token.
+                    window.open(url, "_self");
+                }
+            }
+            else {
+                // the event came from duo, do the post back
+                doPostBack(event.data);
 
-            // always clean up after yourself!
-            offMessage(onReceivedMessage);
+                // always clean up after yourself!
+                offMessage(onReceivedMessage);
+            }
         }
+    }
+
+    /**
+     * Validate that this passed in URL is one that we will actually allow to
+     * be opened.
+     * @param url String URL that the message poster wants to open
+     * @returns {boolean} true if we allow this url to be opened in the window
+     */
+    function isValidUrlToOpen(url) {
+        if (!url) {
+            return false;
+        }
+
+        var parser = document.createElement('a');
+        parser.href = url;
+
+        if (parser.protocol === "duotrustedendpoints:") {
+            return true;
+        } else if (parser.protocol !== "https:") {
+            return false;
+        }
+
+        for (var i = 0; i < VALID_OPEN_WINDOW_DOMAINS.length; i++) {
+           if (parser.hostname.endsWith("." + VALID_OPEN_WINDOW_DOMAINS[i]) ||
+                   parser.hostname === VALID_OPEN_WINDOW_DOMAINS[i]) {
+               return true;
+           }
+        }
+        return false;
     }
 
     /**
@@ -308,7 +375,7 @@ window.Duo = (function(document, window) {
         iframe.src = [
             'https://', host, '/frame/web/v1/auth?tx=', duoSig,
             '&parent=', encodeURIComponent(document.location.href),
-            '&v=2.3'
+            '&v=2.6'
         ].join('');
 
         // listen for the 'message' event
@@ -353,14 +420,11 @@ window.Duo = (function(document, window) {
         }
     }
 
-    // when the DOM is ready, initialize
-    // note that this will get cleaned up if the user calls init directly!
-    onReady(init);
-
     return {
         init: init,
+        _onReady: onReady,
         _parseSigRequest: parseSigRequest,
         _isDuoMessage: isDuoMessage,
         _doPostBack: doPostBack
     };
-}(document, window));
+}));
